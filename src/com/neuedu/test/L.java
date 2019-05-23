7d885ac74a8d85f0bd8d5d8231d73c391fab0a7e
1668488667 <1668488667@qq.com>
2019-05-23 20:01:48 +0800

    bbgg

--- src/com/neuedu/test/L.java
+++ src/com/neuedu/test/L.java
@@ -1,7 +1,10 @@
 package com.neuedu.test;
 
 public class L {
-    public static void main(String[] args) {
-        System.out.println(1);
+    public static void main(String[] arg) {
+        int a[]={3,6,9};
+        for(int i=a.length-1; i>=0; i--) {
+            System.out.println(a[i]);
+        }
     }
 }
